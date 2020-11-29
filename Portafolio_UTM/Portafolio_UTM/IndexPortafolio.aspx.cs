1f1ec90ca70bdc5f39a3b5bac6fed76abeddb261
beds10 <73314223+beds10@users.noreply.github.com>
2020-11-29 17:09:10 -0600

    actualizacion envio correo
    se agrego envio de correo

--- Portafolio_UTM/Portafolio_UTM/IndexPortafolio.aspx.cs
+++ Portafolio_UTM/Portafolio_UTM/IndexPortafolio.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,6 +13,56 @@ namespace Portafolio_UTM
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+
+        protected void LimpiarControles()
+        {
+            txt_name.Text = "";
+            txt_email.Text = "";
+            txt_numero.Text = "";
+            txt_mensaje.InnerText = "";
+
+        }
+        protected void registrar_click(object sender, EventArgs e)
+        {
+            try
+            {
+                string mail2 = txt_email.Text;
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress("blim2023diaz@gmail.com");
+                    mail.To.Add("blim2023diaz@gmail.com");
+                    mail.Subject = "Recibiste una solicitud de contacto - PORTAFOLIOUTM 🤓";
+                    mail.Body = string.Format("Hola haz recibido una solicitud de {0}.<br />Numero de Contacto: {1} <br />Correo Electronico: {2}<br /><br /> Mensaje: {3} ", txt_name.Text, txt_numero.Text, txt_email.Text, txt_mensaje.InnerText);
+                    mail.IsBodyHtml = true;
+
+
+
+                    using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtp.UseDefaultCredentials = false;
+                        smtp.EnableSsl = true;
+
+
+                        smtp.Credentials = new NetworkCredential("blim2023diaz@gmail.com", "Os7&CkfUAo7t#)IKGkYX");
+
+                        smtp.Send(mail);
+                    }
+                }
+                LimpiarControles();
+
+
+                ClientScript.RegisterStartupScript(this.GetType(), "randomtext", "registroexitoso()", true);
+            }
+            catch (Exception)
+            {
+
+            }
+
+
+
 
         }
     }
--- Portafolio_UTM/Portafolio_UTM/IndexPortafolio.aspx.designer.cs
+++ Portafolio_UTM/Portafolio_UTM/IndexPortafolio.aspx.designer.cs
@@ -22,5 +22,86 @@ namespace Portafolio_UTM
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control txt_name.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_name;
+
+        /// <summary>
+        /// Control RegularExpressionValidator3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator3;
+
+        /// <summary>
+        /// Control txt_email.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_email;
+
+        /// <summary>
+        /// Control regexEmailValid.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator regexEmailValid;
+
+        /// <summary>
+        /// Control txt_numero.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_numero;
+
+        /// <summary>
+        /// Control RegularExpressionValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator1;
+
+        /// <summary>
+        /// Control txt_mensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTextArea txt_mensaje;
+
+        /// <summary>
+        /// Control sendMessageButton.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button sendMessageButton;
+
+        /// <summary>
+        /// Control ScriptManager1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
     }
 }
